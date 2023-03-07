a386f4dad21499485d81f308fd85cf63ec478799
BATU1579 <batu1234567890@outlook.com>
2023-03-07 20:51:55 +0800

    ✨ feat: 添加数据表基类

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -2,9 +2,9 @@ from typing import Iterable
 
 from sqlalchemy.engine import Engine
 from sqlalchemy.engine.url import URL
-from sqlalchemy import create_engine, String
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, BigInteger, DateTime, func
 
 from app.util.env import (
     DB_HOST,
@@ -14,7 +14,19 @@ from app.util.env import (
     DB_NAME,
 )
 
-Base = declarative_base()
+
+class Base(declarative_base()):
+    __abstract__ = True
+
+    id = Column(BigInteger, primary_key=True, index=True)
+
+    created_at = Column(DateTime, server_default=func.now(), comment="创建时间")
+    updated_at = Column(
+        DateTime, server_default=func.now(), onupdate=func.now(), comment="更新时间"
+    )
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__}: {self.id}>"
 
 
 class _DataBaseEngine:
