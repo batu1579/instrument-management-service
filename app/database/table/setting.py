5eb61aff12d223587d0def67101213be7bc7fb22
BATU1579 <batu1234567890@outlook.com>
2023-03-07 22:38:23 +0800

    ✨ feat: 添加设置项值类型字段

--- app/database/table/setting.py
+++ app/database/table/setting.py
@@ -1,14 +1,30 @@
+from enum import Enum
+
 from sqlalchemy import Column, String
+from sqlalchemy import Enum as SQLAlchemyEnum
 
 from app.database import Base
 from app.util.string_length import SHORT_LENGTH, LONG_LENGTH
 
 
+class SettingValueType(Enum):
+    STRING = 0
+    INTEGER = 1
+    FLOAT = 2
+    BOOLEAN = 3
+
+
 class Setting(Base):
     __tablename__ = "setting"
 
+    value_type = Column(
+        SQLAlchemyEnum(SettingValueType),
+        nullable=False,
+        default=SettingValueType.STRING,
+        comment="设置项值类型",
+    )
     setting_key = Column(
         String(SHORT_LENGTH), unique=True, nullable=False, comment="设置项名称"
     )
-    setting_value = Column(String(SHORT_LENGTH), nullable=False, comment="设置项值")
+    setting_value = Column(String(LONG_LENGTH), nullable=True, comment="设置项值")
     setting_comment = Column(String(LONG_LENGTH), nullable=True, comment="设置项说明")
