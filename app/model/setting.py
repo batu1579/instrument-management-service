58467cb29e7e59a1282242799dc380f8ad21dd8f
BATU1579 <batu1234567890@outlook.com>
2023-03-08 21:23:38 +0800

    ✨ feat: 添加设置模型的描述和校验

--- app/model/setting.py
+++ app/model/setting.py
@@ -1,18 +1,88 @@
-from typing import Optional
+from typing import Optional, TypeAlias
 
-from pydantic import Field
+from pydantic import Field, validator
 
 from app.model.response import Success
 from app.model.base import BaseModel, DataModel, InDBModel
 from app.util.string_length import SHORT_LENGTH, LONG_LENGTH
 from app.database.table.setting import SettingValueType
 
+ParsedValue: TypeAlias = str | int | float | bool | None
+
 
 class _BaseSetting(DataModel):
-    value_type: SettingValueType
-    setting_key: str = Field(max_length=SHORT_LENGTH)
-    setting_value: Optional[str] = Field(None, max_length=LONG_LENGTH)
-    setting_comment: Optional[str] = Field(None, max_length=LONG_LENGTH)
+    value_type: SettingValueType = Field(
+        ...,
+        title="设置项数据的类型",
+        description="""
+        目前支持的类型有四个，默认为字符串类型，请求时只需携带对应的数值:
+
+            - STRING    (0): 字符串
+            - INTEGER   (1): 整数
+            - FLOAT     (2): 浮点数
+            - BOOLEAN   (3): 布尔值
+
+        其他类型数值（例如数组）请使用字符串类型并编写对应的解析和校验方法。""",
+        example=0,
+    )
+    setting_key: str = Field(
+        ...,
+        max_length=SHORT_LENGTH,
+        regex=r"^[0-9A-Z_]*$",
+        title="设置项键名",
+        description="所有设置项键名都只能由数字，大写字母和下划线组成",
+        example="EXAMPLE_SETTING_KEY",
+    )
+    setting_value: str = Field(
+        ...,
+        max_length=LONG_LENGTH,
+        title="设置项值",
+        description="设置项的值，必须与 value_type 字段对应，如果不能直接强制转换则会抛出一个 ValueError 异常。",
+        example="EXAMPLE_SETTING_VALUE",
+    )
+    value: ParsedValue = Field(
+        None,
+        title="解析后的设置项值",
+        description="辅助字段，用于记录转换类型后的设置项值。创建时不需包含此字段，序列化时也不会包含此字段。",
+        exclude=True,
+    )
+    setting_comment: Optional[str] = Field(
+        None,
+        max_length=LONG_LENGTH,
+        title="设置项备注",
+        description="描述设置项的作用，格式要求或者示例",
+        example="一个设置项描述示例",
+    )
+
+    @validator("value", always=True)
+    def check_value(cls, value: str, all_values: dict) -> ParsedValue:
+        """校验设置值
+
+        Args:
+            value (str): 设置值（必为空）
+            all_values (dict): 全部字段
+
+        Raises:
+            ValueError: 当类型与值不匹配时抛出异常
+
+        Returns:
+            ParsedValue: 转换类型后的设置值
+        """
+        value_type: SettingValueType = all_values["value_type"]
+        raw_value: str = all_values["setting_value"]
+
+        try:
+            if value_type == SettingValueType.INTEGER:
+                return int(raw_value)
+            if value_type == SettingValueType.FLOAT:
+                return float(raw_value)
+            if value_type == SettingValueType.BOOLEAN:
+                return bool(raw_value)
+            return value  # 默认类型不需要转换
+        except ValueError as err:
+            raise ValueError(
+                f"Setting value doesn't match the type: {value_type}"
+            ) from err
 
 
 class Setting(_BaseSetting):
@@ -20,7 +90,7 @@ class Setting(_BaseSetting):
 
 
 class SettingInDB(InDBModel, _BaseSetting):
-    pass
+    setting_value: str = Field(..., max_length=LONG_LENGTH, description="设置项值")
 
 
 class SettingInCreate(_BaseSetting):
@@ -28,7 +98,9 @@ class SettingInCreate(_BaseSetting):
 
 
 class SettingInUpdate(BaseModel):
-    setting_value: Optional[str] = Field(None, max_length=LONG_LENGTH)
+    setting_value: Optional[str] = Field(
+        ..., max_length=LONG_LENGTH, description="Setting value"
+    )
 
 
 class SettingInResponse(Success):
