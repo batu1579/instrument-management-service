202755cbffa0dce077a0c474ca0f2c4c0de97ae8
BATU1579 <batu1234567890@outlook.com>
2023-03-13 21:04:43 +0800

    ✨ feat: 添加 GUID 校验方法

--- app/util/guid.py
+++ app/util/guid.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import TypeVar, Type
 
 from time import strftime, localtime
@@ -7,6 +8,7 @@ from loguru import logger
 from snowflake import client
 from snowflake.server.generator import EPOCH_TIMESTAMP
 
+from app.model.validator import Validated, Validator
 from app.util.env import ID_SERVICE_HOST, ID_SERVICE_PORT
 
 
@@ -17,10 +19,10 @@ def init_snowflake_client() -> None:
     logger.info(f"id service status: {client.get_stats()}")
 
 
-T = TypeVar("T")
+V, T = TypeVar("V"), TypeVar("T")
 
 
-class GUID:
+class GUID(Validated):
     """使用雪花算法生成的全局唯一识别码，依赖于 pysnowflake 生成
 
     :| 1 bit |                   41 bit                  |   2 bit    |  8 bit   |    12 bit    |
@@ -50,7 +52,7 @@ class GUID:
         self.__id = guid
 
     @classmethod
-    def generate(cls: Type[T]) -> T:
+    def generate(cls: Type[GUID]) -> GUID:
         """从服务端获取新的 GUID
 
         Returns:
@@ -59,7 +61,7 @@ class GUID:
         return cls(client.get_guid(), need_varification=False)
 
     @classmethod
-    def parse_str(cls: Type[T], guid: str) -> T:
+    def parse_str(cls: Type[GUID], guid: str) -> GUID:
         """将 GUID 字符串转换为 GUID 对象
 
         Args:
@@ -69,6 +71,43 @@ class GUID:
             T: 新的 GUID 对象
         """
         return cls(int(guid))
+
+    @classmethod
+    def validator(cls: Type[GUID], guid: int | str | GUID) -> GUID:
+        """验证不同类型，同时转换为 GUID 对象
+
+        Args:
+            guid (int | str | GUID): 要转换的数据
+
+        Raises:
+            TypeError: 不可转换的数据类型
+
+        Returns:
+            GUID: 转换后的对象
+        """
+        if isinstance(guid, int):
+            return cls(guid)
+        if isinstance(guid, str):
+            return cls.parse_str(guid)
+        if isinstance(guid, cls):
+            return guid
+
+        raise TypeError(f"Unsupported type to GUID: {type(guid)}")
+
+    @classmethod
+    def __get_validators__(
+        cls: Type[GUID],
+    ) -> Validator[int | str | GUID, GUID]:
+        yield cls.validator
+
+    @classmethod
+    def __modify_schema__(cls: Type[GUID], field_schema: dict) -> None:
+        field_schema.update(
+            {
+                "type": "string",
+                "example": GUID.generate().to_string(),
+            }
+        )
 
     @property
     def guid(self) -> int:
