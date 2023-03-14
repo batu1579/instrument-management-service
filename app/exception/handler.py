f7d3f073003663577402effa97b698b4001987cb
BATU1579 <batu1234567890@outlook.com>
2023-03-14 13:02:29 +0800

    🐞 fix: 修改异常对象传参方式

--- app/exception/handler.py
+++ app/exception/handler.py
@@ -14,7 +14,7 @@ async def invalid_param_handler(req: Request, exc: RequestInvalid) -> JSONRespon
     """非法请求参数异常处理器
 
     Args:
-        request (Request): 请求原文
+        req (Request): 请求原文
         exc (RequestInvalid): 引发的异常对象
 
     Returns:
@@ -24,9 +24,11 @@ async def invalid_param_handler(req: Request, exc: RequestInvalid) -> JSONRespon
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         content=jsonable_encoder(
             Error(
-                code=422,
-                msg="The request arguments are invalid.",
-                info={"detail": exc.errors(), "body": exc.body},
+                **{
+                    "code": 422,
+                    "msg": "The request arguments are invalid.",
+                    "info": {"detail": exc.errors(), "body": exc.body},
+                }
             )
         ),
     )
@@ -36,7 +38,7 @@ async def http_exception_handler(req: Request, exc: HTTPException) -> JSONRespon
     """HTTP 异常处理器
 
     Args:
-        request (Request): 请求原文
+        req (Request): 请求原文
         exc (HTTPException): 引发的异常对象
 
     Returns:
@@ -46,9 +48,11 @@ async def http_exception_handler(req: Request, exc: HTTPException) -> JSONRespon
         status_code=exc.status_code,
         content=jsonable_encoder(
             Error(
-                code=exc.status_code,
-                msg=exc.detail,
-                info="https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Status",
+                **{
+                    "code": exc.status_code,
+                    "msg": exc.detail,
+                    "info": "https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Status",
+                }
             )
         ),
     )
@@ -58,7 +62,7 @@ async def other_exception_handler(req: Request, exc: Exception) -> JSONResponse:
     """其他异常处理器
 
     Args:
-        request (Request): 请求原文
+        req (Request): 请求原文
         exc (Exception): 引发的异常对象
 
     Returns:
@@ -68,9 +72,11 @@ async def other_exception_handler(req: Request, exc: Exception) -> JSONResponse:
         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
         content=jsonable_encoder(
             Error(
-                code=500,
-                msg="Unknown server exception",
-                info="Please contact administrator to report this error.",
+                **{
+                    "code": 500,
+                    "msg": "Unknown server exception",
+                    "info": "Please contact administrator to report this error.",
+                }
             )
         ),
     )
